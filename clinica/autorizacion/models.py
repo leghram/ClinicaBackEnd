c96384724845420f25f99b52735625b822869f74
Miguel PL <mig.parlop@gmail.com>
2022-08-19 18:33:19 -0500

    relaciones FK

--- clinica/autorizacion/models.py
+++ clinica/autorizacion/models.py
@@ -1,19 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from categorias.models import Categoria
 
 
 class ControlCreacionUsuarios(BaseUserManager):
-    def create_user(self, correo, nombre, apellido, password):
+    def create_user(self, correo, nombre, apellido, password, categoriaid):
         if not correo:
             raise ValueError('el usuario necesita un correo')
         correo = self.normalize_email(correo)
-        nuevoUsuario = self.model(correo= correo, nombre = nombre, apellido = apellido)
+        nuevoUsuario = self.model(correo= correo, nombre = nombre, apellido = apellido, categoriaid= categoriaid)
         nuevoUsuario.set_password(password)
         nuevoUsuario.save()
         return nuevoUsuario
 
-    def create_superuser(self,correo,nombre, apellido, password):
-        nuevoUsuario = self.create_user(correo=correo, nombre=nombre, apellido=apellido, password=password)
+    def create_superuser(self,correo,nombre, apellido, password,categoriaid):
+        nuevoUsuario = self.create_user(correo=correo, nombre=nombre, apellido=apellido, password=password, categoriaid=categoriaid)
         nuevoUsuario.is_superuser = True
         nuevoUsuario.is_staff = True
 
@@ -29,6 +30,7 @@ class Usuario(AbstractBaseUser):
     apellido = models.CharField(max_length=60)
     correo = models.EmailField(unique=True)
     password = models.TextField(db_column='contrasena')
+    categoriaid= models.ForeignKey(to=Categoria, related_name='relacioncategorias', db_column='categoria_id', on_delete=models.CASCADE)
 
     is_staff = models.BooleanField(default=False)
     is_active= models.BooleanField(default=True)
@@ -38,7 +40,7 @@ class Usuario(AbstractBaseUser):
     USERNAME_FIELD = 'correo'
 
 
-    REQUIRED_FIELDS= ['nombre','apellido']
+    REQUIRED_FIELDS= ['nombre','apellido','categoriaid']
 
     class Meta:
         db_table = 'usuarios'
--- clinica/especialidades/models.py
+++ clinica/especialidades/models.py
@@ -4,6 +4,7 @@ class Especialidad(models.Model):
 
     id= models.AutoField(primary_key=True, unique=True)
     nombre = models.CharField(max_length=60)
+    descripcion = models.TextField()
 
     class Meta:
         db_table='especialidades'
